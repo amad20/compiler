175fa60e1d5add0defa935c59239987160514621
amad20 <120679488+amad20@users.noreply.github.com>
2023-12-16 18:30:41 -0600

    Update staticSemantics.c

--- staticSemantics.c
+++ staticSemantics.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,3 +41,37 @@ void insertVariableDeclarations(Node *node) {
     if (!node) return;
 
     if (strcmp(node->name, "<varList>") == 0) {
+        insertVariable(node->token.instance); // Assuming token.instance holds the variable name
+    }
+
+    // Recursively call on children
+    insertVariableDeclarations(node->child1);
+    insertVariableDeclarations(node->child2);
+    insertVariableDeclarations(node->child3);
+    insertVariableDeclarations(node->child4);
+}
+
+void verifyVariableUsage(Node *node) {
+    if (!node) return;
+
+    if (strcmp(node->name, "<identifier>") == 0) {
+        if (!verifyVariable(node->token.instance)) {
+            fprintf(stderr, "Semantic Error: Variable %s not declared.\n", node->token.instance);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Recursively call on children, skipping <vars> subtree
+    if (strcmp(node->name, "<vars>") != 0) {
+        verifyVariableUsage(node->child1);
+        verifyVariableUsage(node->child2);
+        verifyVariableUsage(node->child3);
+        verifyVariableUsage(node->child4);
+    }
+}
+
+void staticSemantics(Node *root) {
+    insertVariableDeclarations(root); // First pass for insertion
+    verifyVariableUsage(root); // Second pass for verification
+printf("Static semantics checks passed.\n");
+}
